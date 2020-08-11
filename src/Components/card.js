64bb9e22b60b222af4b152d035dc468c831027aa
seriogavolkov <serioga.volkov@gmail.com>
2020-08-10 20:33:35 -0600

    onmouse

--- src/Components/card.js
+++ src/Components/card.js
@@ -1,14 +1,29 @@
-import React, {} from 'react'
+import React, {useState, useEffect} from 'react'
 
 
 
-const CardUs = ({src, typo, alt}) => {
+const CardUs = ({src, alt, typo}) => {
+    const [toggledstyle, setToggle] = useState(false);
+    const [nostyle, setStyle] = useState('40');
+    const onEnterEvent = '80'
+
+    const styleHandler = () => {
+        setToggle(!toggledstyle);
+        if(!toggledstyle){
+            setStyle('40')
+        }else {
+            setStyle(onEnterEvent)
+        }
+
+    }
+    console.log(nostyle)
     return (
         <>
         <div className='cardcomp'>
-           <img src={src} alt={alt} width='40' height='40'></img>
+           <img src={src} alt={alt} width='40' height={nostyle} onMouseOver={styleHandler} ></img>
            <p>{typo}</p>
         </div>
+       
         </>
     )
 }
